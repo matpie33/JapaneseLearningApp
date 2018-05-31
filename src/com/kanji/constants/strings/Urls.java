d0b058484505886536d4b3fd58dfec6f9596d613
matpie33 <Spaaw@poczta.fm>
2018-05-31 13:15:11 +0200

    Replace problematic english dictionary with other.

--- src/com/kanji/constants/strings/Urls.java
+++ src/com/kanji/constants/strings/Urls.java
@@ -1,7 +1,7 @@
 package com.kanji.constants.strings;
 
 public class Urls {
-	public static final String DICTIONARY_PL_EN_MAIN_PAGE = "https://pl.bab.la/slownik/polski-angielski/";
+	public static final String DICTIONARY_PL_EN_MAIN_PAGE = "https://ling.pl/slownik/polsko-angielski";
 	public static final String KANJI_KOOHI_LOGIN_PAGE = "https://kanji.koohii.com/account";
 	public static final String KANJI_KOOHI_MAIN_PAGE = "https://kanji.koohii.com/study";
 	public static final String KANJI_KOOHI_REVIEW_BASE_PAGE = "http://kanji.koohii.com/study/kanji/";
