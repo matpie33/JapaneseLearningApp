80e041f5aded15beb5d1dfaec9981727c417994e
matpie33 <Spaaw@poczta.fm>
2018-06-17 18:27:31 +0200

    Change row highlight color.

--- src/com/kanji/constants/Colors.java
+++ src/com/kanji/constants/Colors.java
@@ -8,6 +8,6 @@ public class Colors {
 
 	public static final Color CONTENT_PANEL_COLOR = BasicColors.BLUE_DARK_4;
 	public static final Color BACKGROUND_PANEL_COLOR = BasicColors.BLUE_BRIGHT_2;
-	public static final Color LIST_ROW_HIGHLIGHT_COLOR = BasicColors.BLUE_NORMAL_5;
+	public static final Color LIST_ROW_HIGHLIGHT_COLOR = BasicColors.BLUE_NORMAL_7;
 
 }
