62324f99a7cc5a54e19875993c7b8fd8334d8bcc
matpie33 <Spaaw@poczta.fm>
2018-09-17 22:04:53 +0200

    Fix not loading page.

--- src/com/kanji/constants/strings/Urls.java
+++ src/com/kanji/constants/strings/Urls.java
@@ -4,6 +4,6 @@ public class Urls {
 	public static final String DICTIONARY_PL_EN_MAIN_PAGE = "https://ling.pl/slownik/polsko-angielski";
 	public static final String KANJI_KOOHI_LOGIN_PAGE = "https://kanji.koohii.com/account";
 	public static final String KANJI_KOOHI_MAIN_PAGE = "https://kanji.koohii.com/study";
-	public static final String KANJI_KOOHI_REVIEW_BASE_PAGE = "http://kanji.koohii.com/study/kanji/";
+	public static final String KANJI_KOOHI_REVIEW_BASE_PAGE = "https://kanji.koohii.com/study/kanji/";
 
 }
