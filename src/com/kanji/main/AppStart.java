e11e9b23f6edce76eba8ef7cc529e4b4c5a90dd1
matpie33 <Spaaw@poczta.fm>
2018-09-22 16:09:11 +0200

    Fix not working tangorin dictionary page due to ipv6 conflict. Add vm argument using batch file

--- src/com/kanji/main/AppStart.java
+++ src/com/kanji/main/AppStart.java
@@ -5,19 +5,26 @@ import com.kanji.constants.Colors;
 import com.kanji.windows.ApplicationWindow;
 import com.sun.javafx.application.PlatformImpl;
 
-import javax.swing.*;
-import java.awt.*;
-
 public class AppStart {
 
 	public static void main(String[] args) {
 
-		PlatformImpl.startup(()->{});
+
+		PlatformImpl.startup(() -> {
+		});
 		MainPanel.setDefaultColor(Colors.CONTENT_PANEL_COLOR);
 		//TODO some configuration is needed: panel default color,
 		//buttons color, list default color
 		ApplicationWindow b = new ApplicationWindow();
 		b.initiate();
+		String preferIpV4Property = System
+				.getProperty("java.net.preferIPv4Stack");
+		if (preferIpV4Property == null) {
+			String message = "Aplikacja moze byc uruchomiona tylko z pliku exe.";
+			b.showMessageDialog(message);
+			System.exit(1);
+
+		}
 
 	}
 
