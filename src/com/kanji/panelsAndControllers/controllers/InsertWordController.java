f317dfe741ff2131f758c3ffc3a68225590475c1
matpie33 <Spaaw@poczta.fm>
2018-09-29 19:00:24 +0200

    Refactor insert word panel and controller.

--- src/com/kanji/panelsAndControllers/controllers/InsertWordController.java
+++ src/com/kanji/panelsAndControllers/controllers/InsertWordController.java
@@ -1,13 +1,16 @@
 package com.kanji.panelsAndControllers.controllers;
 
+import com.guimaker.panels.MainPanel;
+import com.kanji.constants.enums.InputGoal;
 import com.kanji.constants.strings.ExceptionsMessages;
 import com.kanji.list.listElements.ListElement;
 import com.kanji.list.listeners.InputValidationListener;
+import com.kanji.list.myList.ListRowData;
 import com.kanji.list.myList.MyList;
 import com.kanji.model.PropertyPostValidationData;
 import com.kanji.panelsAndControllers.panels.InsertWordPanel;
+import com.kanji.utilities.CommonListElements;
 import com.kanji.utilities.ThreadUtilities;
-import com.kanji.windows.DialogWindow;
 
 import javax.swing.*;
 import javax.swing.FocusManager;
@@ -19,71 +22,60 @@ public class InsertWordController<Word extends ListElement>
 		implements InputValidationListener<Word> {
 
 	private MyList<Word> list;
-	private DialogWindow parentDialog;
 	private ApplicationController applicationController;
-	private InsertWordPanel<Word> insertWordPanel;
+	private InsertWordPanel<Word> panel;
 	private boolean addingWordWasRequested = false;
+	private Word word;
 
 	public InsertWordController(MyList<Word> list,
 			ApplicationController applicationController,
-			InsertWordPanel insertWordPanel) {
-		this.insertWordPanel = insertWordPanel;
+			InsertWordPanel panel) {
+		this.panel = panel;
 		this.list = list;
 		this.applicationController = applicationController;
+		list.getListRowCreator().addValidationListener(this);
 	}
 
-	public void setParentDialog(DialogWindow parent) {
-		parentDialog = parent;
-	}
-
-	private void addWordIfItsNew(Word word) {
+	private void checkIfWordExistsOrIsEmptyAndAdd(Word word) {
 		if (word.isEmpty()) {
-			parentDialog
+			panel.getDialog()
 					.showMessageDialog(ExceptionsMessages.NO_INPUT_SUPPLIED);
 			return;
 		}
 		ThreadUtilities.callOnOtherThread(() -> {
-			boolean addedWord = addWordToList(word);
+			boolean addedWord = list.addWord(word);
 			if (addedWord) {
+				list.scrollToBottom();
 				applicationController.saveProject();
+			}
+			else{
+				list.highlightRow(list.get1BasedRowNumberOfWord(word) - 1, true);
+				panel.getDialog().showMessageDialog(
+						String.format(ExceptionsMessages.WORD_ALREADY_EXISTS,
+								list.get1BasedRowNumberOfWord(word)), false);
 			}
 		});
 	}
 
-	private boolean addWordToList(Word word) {
-		boolean addedWord = list.addWord(word);
-		if (addedWord) {
-			list.scrollToBottom();
-			//TODO remove from this method show message - it should just add word and return boolean
-		}
-		else {
-			list.highlightRow(list.get1BasedRowNumberOfWord(word) - 1, true);
-			parentDialog.showMessageDialog(
-					String.format(ExceptionsMessages.WORD_ALREADY_EXISTS,
-							list.get1BasedRowNumberOfWord(word)), false);
-		}
-		return addedWord;
-	}
-
-	public AbstractAction createActionValidateFocusedElement() {
+	public AbstractAction createActionValidateAndAddFocusedElement() {
 		return new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				Component focusOwner = FocusManager.getCurrentManager()
 						.getFocusOwner();
 				if (focusOwner instanceof JTextComponent) {
-					validateFocusedElement();
+					validateFocusedTextInput();
 					addingWordWasRequested = true;
 				}
 				else {
-					addWord();
+					checkWordAndAddAndReinitializePanel();
 				}
 			}
 		};
 
 	}
 
-	private void validateFocusedElement() {
+	private void validateFocusedTextInput() {
 		KeyboardFocusManager.getCurrentKeyboardFocusManager()
 				.clearGlobalFocusOwner();
 	}
@@ -92,13 +84,23 @@ public class InsertWordController<Word extends ListElement>
 	public <WordProperty> void inputValidated(
 			PropertyPostValidationData<WordProperty, Word> postValidationData) {
 		if (addingWordWasRequested && postValidationData.isValid()) {
-			addWord();
+			checkWordAndAddAndReinitializePanel();
 		}
 		addingWordWasRequested = false;
 	}
 
-	private void addWord() {
-		addWordIfItsNew(insertWordPanel.getWord());
-		insertWordPanel.reinitializePanel();
+	private void checkWordAndAddAndReinitializePanel() {
+		checkIfWordExistsOrIsEmptyAndAdd(word);
+		panel.reinitializePanel();
 	}
+
+	public MainPanel createListRowPanel() {
+		word = list.getWordInitializer().initializeElement();
+		ListRowData<Word> listRowData = list.getListRowCreator()
+				.createListRow(word, CommonListElements
+								.forSingleRowOnly(panel.getLabelsColor()),
+						InputGoal.ADD);
+		return listRowData.getRowPanel();
+	}
+
 }
--- src/com/kanji/panelsAndControllers/panels/InsertWordPanel.java
+++ src/com/kanji/panelsAndControllers/panels/InsertWordPanel.java
@@ -1,18 +1,13 @@
 package com.kanji.panelsAndControllers.panels;
 
 import com.guimaker.enums.FillType;
-import com.guimaker.panels.MainPanel;
 import com.guimaker.row.SimpleRowBuilder;
-import com.kanji.constants.enums.InputGoal;
 import com.kanji.constants.strings.ButtonsNames;
 import com.kanji.constants.strings.HotkeysDescriptions;
 import com.kanji.list.listElements.ListElement;
-import com.kanji.list.myList.ListRowData;
 import com.kanji.list.myList.MyList;
 import com.kanji.panelsAndControllers.controllers.InsertWordController;
-import com.kanji.utilities.CommonListElements;
 import com.kanji.windows.ApplicationWindow;
-import com.kanji.windows.DialogWindow;
 
 import javax.swing.*;
 import java.awt.*;
@@ -22,52 +17,30 @@ public class InsertWordPanel<Word extends ListElement>
 		extends AbstractPanelWithHotkeysInfo {
 
 	private InsertWordController<Word> controller;
-	private Word word;
-	private MyList<Word> wordsList;
 	private Color labelsColor = Color.WHITE;
 
 	public InsertWordPanel(MyList<Word> list,
 			ApplicationWindow applicationWindow) {
-		wordsList = list;
 		controller = new InsertWordController<>(list,
 				applicationWindow.getApplicationController(), this);
-
-	}
-
-	@Override
-	public void setParentDialog(DialogWindow parentDialog) {
-		super.setParentDialog(parentDialog);
-		controller.setParentDialog(parentDialog);
 		initializeOneTimeOnlyElements();
 	}
 
+	public Color getLabelsColor() {
+		return labelsColor;
+	}
+
 	private void initializeOneTimeOnlyElements() {
-		wordsList.getListRowCreator().addValidationListener(controller);
 		AbstractButton cancel = createButtonClose();
-		AbstractButton approve = createButtonValidate();
+		AbstractButton approve = createButtonValidateAndAddWord();
 		setNavigationButtons(cancel, approve);
 	}
 
 	@Override
 	public void createElements() {
-		initializeWord();
-		initializeAddWordPanel();
-	}
-
-	private void initializeWord() {
-		word = wordsList.getWordInitializer().initializeElement();
-	}
-
-	private void initializeAddWordPanel() {
-
-		ListRowData<Word> listRowData = wordsList.getListRowCreator()
-				.createListRow(word,
-						CommonListElements.forSingleRowOnly(labelsColor),
-						InputGoal.ADD);
-		MainPanel addWordPanel = listRowData.getRowPanel();
-		mainPanel.addRow(SimpleRowBuilder
-				.createRow(FillType.BOTH, addWordPanel.getPanel())
-				.fillAllVertically().fillHorizontallyEqually());
+		mainPanel.addRow(SimpleRowBuilder.createRow(FillType.BOTH,
+				controller.createListRowPanel().getPanel()).fillAllVertically()
+				.fillHorizontallyEqually());
 	}
 
 	public void reinitializePanel() {
@@ -76,16 +49,12 @@ public class InsertWordPanel<Word extends ListElement>
 		mainPanel.updateView();
 	}
 
-	private AbstractButton createButtonValidate() {
+	private AbstractButton createButtonValidateAndAddWord() {
 		//if combobox is selected, the enter key will not work, because it has
 		//another function there - choose the currently selected item
 		return createButtonWithHotkey(KeyEvent.VK_ENTER,
-				controller.createActionValidateFocusedElement(),
+				controller.createActionValidateAndAddFocusedElement(),
 				ButtonsNames.ADD, HotkeysDescriptions.ADD_WORD);
-	}
-
-	public Word getWord() {
-		return word;
 	}
 
 }
