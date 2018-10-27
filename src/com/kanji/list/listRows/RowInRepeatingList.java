0a45671b4d26ebf9a7cf7a5cda9689c4a468ea0b
matpie33 <Spaaw@poczta.fm>
2018-10-27 20:48:04 +0200

    Improve naming for row in repeating list creator.

--- src/com/kanji/list/listRows/RowInRepeatingList.java
+++ src/com/kanji/list/listRows/RowInRepeatingList.java
@@ -23,45 +23,44 @@ import java.time.format.DateTimeFormatter;
 
 public class RowInRepeatingList implements ListRowCreator<RepeatingData> {
 
+	public static final String DATE_FORMAT = "dd MMMM yyyy / HH:mm";
 	private final Color labelsColor = Color.WHITE;
 
 	@Override
 	public ListRowData createListRow(RepeatingData repeatingData,
 			CommonListElements commonListElements, InputGoal inputGoal) {
-		String word = repeatingData.getRepeatingRange();
-		String time = repeatingData.getTimeSpentOnRepeating();
-		LocalDateTime date1 = repeatingData.getRepeatingDate();
+		String repeatingRange = repeatingData.getRepeatingRange();
+		String timeSpent = repeatingData.getTimeSpentOnRepeating();
+		LocalDateTime date = repeatingData.getRepeatingDate();
 
-		JLabel repeatedWords = GuiElementsCreator.createLabel(
+		JLabel rangeOfRepeatedWordsLabel = GuiElementsCreator.createLabel(
 				new ComponentOptions().text(
-						Prompts.REPEATING_WORDS_RANGE + word)
+						Prompts.REPEATING_WORDS_RANGE + repeatingRange)
 									  .foregroundColor(labelsColor));
 
-		DateTimeFormatter sdf = DateTimeFormatter.ofPattern(
-				"dd MMMM yyyy / HH:mm");
-		JLabel date = GuiElementsCreator.createLabel(
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+		JLabel dateLabel = GuiElementsCreator.createLabel(
 				new ComponentOptions().text(
-						Prompts.REPEATING_DATE + sdf.format(date1))
-									  .foregroundColor(labelsColor));
+						Prompts.REPEATING_DATE + formatter.format(date))
+									  .foregroundColor(BasicColors.BLUE_NORMAL_2));
 
-		date.setForeground(BasicColors.BLUE_NORMAL_2);
-		JLabel timeSpent = null;
+		JLabel timeSpentLabel = null;
 
-		if (time != null) {
-			timeSpent = GuiElementsCreator.createLabel(
+		if (timeSpent != null) {
+			timeSpentLabel = GuiElementsCreator.createLabel(
 					new ComponentOptions().text(
-							String.format(Prompts.REPEATING_TIME, time))
+							String.format(Prompts.REPEATING_TIME, timeSpent))
 										  .foregroundColor(labelsColor));
 		}
 
-		AbstractButton delete = commonListElements.getButtonDelete();
+		AbstractButton deleteButton = commonListElements.getButtonDelete();
 
 		MainPanel panel = new MainPanel();
 		panel.addRows(SimpleRowBuilder.createRow(FillType.HORIZONTAL,
-				commonListElements.getRowNumberLabel(), date)
-									  .nextRow(repeatedWords)
-									  .nextRow(timeSpent)
-									  .nextRow(FillType.NONE, delete));
+				commonListElements.getRowNumberLabel(), dateLabel)
+									  .nextRow(rangeOfRepeatedWordsLabel)
+									  .nextRow(timeSpentLabel)
+									  .nextRow(FillType.NONE, deleteButton));
 
 		if (!inputGoal.equals(InputGoal.EDIT)) {
 			//TODO implement it for searching repeating list to work
