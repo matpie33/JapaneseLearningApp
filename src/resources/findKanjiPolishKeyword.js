c053f69044a9078720f7a3870dad3fc0fb3d9e4a
matpie33 <Spaaw@poczta.fm>
2019-01-12 22:27:55 +0100

    Show tooltip with kanji keyword polish meaning.

--- src/resources/findKanjiPolishKeyword.js
+++ src/resources/findKanjiPolishKeyword.js
@@ -5,5 +5,38 @@ var selection = window.getSelection().toString().toLowerCase();
 var heisigKanjiId = keywords.indexOf(selection) + 1;
 if (heisigKanjiId >= 0){
     var polishKeyword = ApplicationController.getKanjiKeywordById(heisigKanjiId);
-    document.getElementById('txtSearch').value = polishKeyword;
+
+    var selection = window.getSelection();
+    range = selection.getRangeAt(0);
+    rect = range.getBoundingClientRect();
+
+    var div = createTooltipDiv(polishKeyword);
+    document.body.appendChild(div);
+
+    var body = document.getElementsByTagName("BODY")[0];
+    body.onmousemove = function (){
+        div.style.display = 'none';
+    }
 }
+
+function createTooltipDiv (polishKeyword){
+    var div = document.createElement('div');
+    div.style.border = '2px solid black';
+    div.style.position = 'fixed';
+    div.style.top = (rect.top - 50) + 'px';
+    div.style.fontSize = '30px';
+    div.style.background = 'white';
+    div.style.wordBreak='break-word';
+    div.style.padding = '0px 4px';
+    if (!polishKeyword || 0 === polishKeyword.length){
+        div.innerHTML = 'Brak informacji';
+    }
+    else{
+        div.innerHTML = polishKeyword;
+    }
+
+    div.style.left = rect.left + 'px';
+    return div;
+}
+
+
